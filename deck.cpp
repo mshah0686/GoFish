7a58fd4212fb7a09613f544ec2de0c821098fa51
mshah0686 <mshah0686@gmail.com>
2019-04-10 19:59:50 -0500

    deck comments

--- deck.cpp
+++ deck.cpp
@@ -2,6 +2,7 @@
 #include "deck.h"
 
 Deck::Deck() {
+    //suit order is spades, hearts, diamonds clubs
     int cardNum = 0;
     for(int i = 1; i < 14; i++, cardNum++) {
         myCards[cardNum] = Card(i, Card::spades);
@@ -15,6 +16,7 @@ Deck::Deck() {
     for(int i = 1; i < 14; i++, cardNum++) {
         myCards[cardNum] = Card(i, Card::diamonds);
     }
+    //top card is index 0
     myIndex = 0;
     srand(time(NULL));
 }
@@ -23,6 +25,7 @@ void Deck::shuffle() {
     if(myIndex >= SIZE) {
         return;
     }
+    //swap two cards at two index size*5 times
     for(int i = 0; i < size() * 5; i++){
         //generate two random indices to swap
         int firstIndex = (rand() % size()) + myIndex;
@@ -34,20 +37,24 @@ void Deck::shuffle() {
 }
 
 Card Deck::dealCard() {
+    //top card is returned, index changed
     if(myIndex < 52) {
         int cpy = myIndex;
         myIndex++;
         return myCards[cpy];
     }
+    //end of deck, no more cards
     cout << "ERROR NO MORE CARDS" << endl;
     exit(-1);
 }
 
 int Deck::size() const {
+    //number of cards left is total - amount dealed
     return SIZE - myIndex;
 }
 
 string Deck::showDeck() {
+    //print the deck (mainly for debugging)
     string ret;
     for(int i = myIndex; i < SIZE; i++){
         ret += myCards[i].toString() + " ";
